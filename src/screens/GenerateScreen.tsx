77c3f613f1172fe2c323c7c201c1c117c0349daa
Nguyen Dung Tuan <dungtuann67@gmail.com>
2023-09-22 16:18:52 +0000

    upload image and create metadata

--- src/screens/GenerateScreen.tsx
+++ src/screens/GenerateScreen.tsx
@@ -14,7 +14,7 @@ import { generate } from "fast-glob/out/managers/tasks";
 type RootStackParamList = {
   GenerateScreen: {};
   ResultsScreen: { prompt: string };
-  MintScreen: {};
+  MintScreen: { imageUrl : string };
   ShareScreen: {};
 };
 const Stack = createStackNavigator<RootStackParamList>();
@@ -53,7 +53,7 @@ function ResultsScreen({
   navigation,
 }: NativeStackScreenProps<RootStackParamList, "ResultsScreen">) {
   const handleNext = () => {
-    navigation.push("MintScreen", {});
+    navigation.push("MintScreen", {imageUrl: imageUrl});
   };
 
   const { prompt } = route.params;
@@ -136,35 +136,134 @@ function MintScreen({
     tag: "",
   });
 
-  const handleFinish = () => {};
+  const { imageUrl } = route.params;
+  
+  
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const uploadImage = async () => {
+    try {
+      const blob  = await axios.get(imageUrl, { responseType: 'arraybuffer' })
+        
+      if (blob.status !== 200) {
+        throw new Error(`Failed to fetch image with status: ${blob.status}`);
+      }
+
+      // Create a Blob from the response data
+      const imageBlob = new Blob([blob.data]);
+
+      // Create a File object from the Blob
+      const imageFile = new File([imageBlob], 'image.jpg', { type: 'image/jpeg' });
+        
+
+      var formdata = new FormData();
+      formdata.append("file", imageFile, "mintImage.jpeg");
+
+      const response = await axios.post(
+        "https://api.shyft.to/sol/v1/storage/upload",
+        formdata,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            "x-api-key": "SdeplwOAeAzCOhEu"
+          }
+        }
+      )
+      return response.data.result;
+      
+    } catch(error) {
+      console.error("Error upload image:", error);
+    } 
+  }
+
+  const handleFinish = async () => {
+    try {
+      setLoading(true);
+      const { uri } = await uploadImage();
+      
+      let body = {
+        "name": metadata.name,
+        "symbol": "SHY",
+        "description": metadata.description,
+        "image": uri,
+        "attributes": [
+          {
+            "trait_type": "fast",
+            "value": "80"
+          },
+          {
+            "trait_type": "efficient",
+            "value": 100
+          }
+        ],
+        "royalty": 5,
+        "creator": "BvzKvn6nUUAYtKu2pH3h5SbUkUNcRPQawg4bURBiojJx",
+        "share": 100,
+        "external_url": "https://www.example.com",
+        "files": [
+          {
+            "uri": "https://nftstorage.link/ipfs/bafybeia4ml3aaj3tqln5z6qxqvi2ygfouw4ppt7t3qp3wrsoiccslexomm",
+            "type": "image/png"
+          },
+          {
+            "uri": "https://nftstorage.link/ipfs/bafybeigvojjdy5ofaeu7semfvdjugnbutda37r35xjpsvmm5vzblill6k4",
+            "type": "image/png"
+          }
+        ]
+      }
+      
+      const response = await axios.post(
+        "https://api.shyft.to/sol/v1/metadata/create",
+        body,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key": "SdeplwOAeAzCOhEu"
+          }
+        }
+      );
+      console.log(response);
+    } catch(error) {
+      console.error("Error mint nft:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <View>
-      <Text variant="titleMedium">Provide information to complete</Text>
-      <TextInput
-        style={{ marginBottom: 5 }}
-        mode="outlined"
-        label="Name"
-        value={metadata.name}
-        onChangeText={(e) => setMetadata({ ...metadata, name: e })}
-      />
-      <TextInput
-        style={{ marginBottom: 5 }}
-        mode="outlined"
-        label="Name"
-        value={metadata.description}
-        onChangeText={(e) => setMetadata({ ...metadata, description: e })}
-      />
-      <TextInput
-        style={{ marginBottom: 15 }}
-        mode="outlined"
-        label="Tag"
-        value={metadata.tag}
-        onChangeText={(e) => setMetadata({ ...metadata, tag: e })}
-      />
-      <Button icon="check" mode="contained" onPress={handleFinish}>
-        Generate
-      </Button>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <>
+          <Text variant="titleMedium">Provide information to complete</Text>
+          <TextInput
+            style={{ marginBottom: 5 }}
+            mode="outlined"
+            label="Name"
+            value={metadata.name}
+            onChangeText={(e) => setMetadata({ ...metadata, name: e })}
+          />
+          <TextInput
+            style={{ marginBottom: 5 }}
+            mode="outlined"
+            label="Description"
+            value={metadata.description}
+            onChangeText={(e) => setMetadata({ ...metadata, description: e })}
+          />
+          <TextInput
+            style={{ marginBottom: 15 }}
+            mode="outlined"
+            label="Tag"
+            value={metadata.tag}
+            onChangeText={(e) => setMetadata({ ...metadata, tag: e })}
+          />
+          <Button icon="check" mode="contained" onPress={handleFinish}>
+            Generate
+          </Button>
+        </>
+      )}
+      
     </View>
   );
 }
